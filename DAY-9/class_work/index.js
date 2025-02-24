22050e2e7a71213ca165492ef85ffd6398b3c136
amarjit2003 <amarjitdas317@gmail.com>
2025-02-24 12:56:35 +0530

    add all

---
+++ DAY-9/class_work/index.js
@@ -0,0 +1,33 @@
+let str = "svu"
+
+console.log(str.length)
+
+console.log(str[0])
+console.log(str[str.lenght-1])
+
+
+
+let ar =[1,2,3, "Amarjit",1.2,true,false,undefined]
+console.log(arr)
+
+// console.log(arr[arr.lenght-1])
+arr.push("Sam")
+
+console.log(arr)
+
+
+// object
+
+
+// object Literal
+// new key word
+// Constructor
+
+let name = "preet"
+let Class = "BCCI"
+let = "FS"
+
+
+let newjob = {name,class,sub}
+
+console.log(newjob)
